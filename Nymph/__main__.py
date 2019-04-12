6d5296c930c91d098fbf5376a6ce422fc7e89e81
MagonxESP <janma.360@gmail.com>
2019-04-12 20:51:40 +0200

    return response

--- Nymph/__main__.py
+++ Nymph/__main__.py
@@ -1,5 +1,6 @@
 from Nymph.nymph import Nymph
-from flask import Flask, request
+from flask import Flask, request, Response
+
 
 app = Flask(__name__)
 nymph_bot = Nymph()
@@ -9,6 +10,9 @@ nymph_bot = Nymph()
 def post():
     if request.method == 'POST':
         nymph_bot.post(request.form['status'])
+        return Response(response=None, status=200)
+    else:
+        return Response(response=None, status=405)
 
 
 app.run(host='0.0.0.0')
