de91948a91ff6751f7518d953dacc1b06a6df12c
MagonxESP <janma.360@gmail.com>
2019-04-13 01:41:17 +0200

    guarda los tweets en un fichero json

--- Nymph/nymph.py
+++ Nymph/nymph.py
@@ -1,5 +1,9 @@
 import Nymph.settings as settings
 import twitter
+import json
+import os
+import time
+import datetime
 
 
 class Nymph:
@@ -31,7 +35,44 @@ class NymphTwitter(ApiService):
         )
 
     def post(self, status, options=None):
-        self.__api.PostUpdate(status)
+        try:
+            api = self.__api
+            posted_status = api.PostUpdate(status)
+            self.saveStatus(posted_status.AsDict())
+        except twitter.error.TwitterError as e:
+            pass
+
+    def saveStatus(self, status):
+        try:
+            tweets = []
+
+            if os.path.isfile('tweets.json'):
+                file = open('tweets.json', 'r')
+                tweets = json.loads(file.read())
+                file.close()
+
+            tweets.append(status)
+            file = open('tweets.json', 'w')
+            file.write(json.dumps(tweets))
+            file.close()
+        except IOError as e:
+            pass
+
+    def getLastTweet(self, status):
+        with open('tweets.json', 'r') as file:
+            tweets = json.loads(file.read())
+
+        tweets.sort(key=lambda item: item['created_at'])
+
+        tweets_same_status = []
+        for tweet in tweets:
+            if tweet['text'] == status:
+                tweets_same_status.append(tweet)
+
+    def createdTimeToTimestamp(self, created_at):
+        # timestamp = time.strftime('%Y-%m-%d %H:%M:%S', time.strptime(created_at, '%a %b %d %H:%M:%S +0000 %Y'))
+        # datetime.datetime.fromisoformat()
+        pass
 
 
 class NymphDiscord(ApiService):
