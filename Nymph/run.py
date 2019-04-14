5cc431379276f987beeb2c42f940127dc93c4776
MagonxESP <janma.360@gmail.com>
2019-04-14 02:08:13 +0200

    run dev server

--- Nymph/run.py
+++ Nymph/run.py
@@ -1,18 +1,4 @@
-from Nymph.nymph import Nymph
-from flask import request, Response
 from Nymph import app
 
-
-nymph_bot = Nymph()
-
-
-@app.route('/post', methods=['POST'])
-def post():
-    if request.method == 'POST':
-        nymph_bot.post(request.form['status'])
-        return Response(response=None, status=200)
-    else:
-        return Response(response=None, status=405)
-
-
-app.run()
+# run Flask development server
+app.run(debug=True)
